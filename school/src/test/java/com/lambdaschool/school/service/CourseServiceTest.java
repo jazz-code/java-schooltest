8275eaac8eddec5a366daf3a0eb028ad6959d3dc
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-18 17:31:57 -0400

    added dependencies, set up for unit testing, added a method for findCourseById in CourseService and CourseServiceImpl

---
+++ school/src/test/java/com/lambdaschool/school/service/CourseServiceTest.java
@@ -0,0 +1,41 @@
+package com.lambdaschool.school.service;
+
+import com.lambdaschool.school.SchoolApplicationTests;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SchoolApplicationTests.class)
+public class CourseServiceTest {
+
+    @Autowired
+    private CourseService courseService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void getCountStudentsInCourse() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
