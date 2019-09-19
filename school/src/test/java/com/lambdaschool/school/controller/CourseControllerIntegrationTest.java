2b8079fbef8e56fcf387050e74377203cb92d4b5
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-18 20:48:00 -0400

    integration test for response time of courses/courses

---
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.lambdaschool.school.controller;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.lessThan;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //set up web environment for random port
+public class CourseControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+
+    // GET /courses/courses/
+    @Test
+    public void measuredResponseTime() {
+        given().when().get("/courses/courses").then().time(lessThan(5000L));
+    }
+
+}
